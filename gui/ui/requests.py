bd48085d912d1a0adfb13bc94e9a5fd2cce2d807
McClawdy <mcclawdy@gmail.com>
2020-10-16 00:59:56 +0300

    ui edded

---
+++ gui/ui/requests.py
@@ -0,0 +1,162 @@
+Request URL: https://sd.korusconsulting.ru/api/v3/tasks?input_data=%7B%22list_info%22%3A%7B%22row_count%22%3A%2225%22%2C%22start_index%22%3A%221%22%2C%22get_total_count%22%3Atrue%2C%22sort_field%22%3A%22scheduled_start_time%22%2C%22sort_order%22%3A%22A%22%2C%22filter%22%3A%225%22%7D%2C%22fields_required%22%3A%5B%22id%22%2C%22title%22%2C%22scheduled_start_time%22%5D%7D&SUBREQUEST=XMLHTTP&_=1602773274806
+Request Method: GET
+Status Code: 200 OK
+Remote Address: 91.231.213.53:443
+Referrer Policy: strict-origin-when-cross-origin
+Content-Length: 436
+Content-Type: application/json;charset=UTF-8
+Date: Thu, 15 Oct 2020 14:47:56 GMT
+Server: Apache-Coyote/1.1
+Accept: v3+json
+Accept-Encoding: gzip, deflate, br
+Accept-Language: en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7
+Cache-Control: max-age=0
+Connection: keep-alive
+Content-Type: application/x-www-form-urlencoded; charset=utf-8
+Cookie: sdpcsrfcookie=41a67e47-e334-40ea-ac48-6b640030a3c5; SDPSESSIONID=A2FF8C6DFE774A5C764E100D04727532; JSESSIONIDSSO=FF8E356119E708D7625ECC6E90CB8806
+DNT: 1
+Host: sd.korusconsulting.ru
+If-Modified-Since: Thu, 1 Jan 1970 00:00:00 GMT
+Referer: https://sd.korusconsulting.ru/HomePage.do?view_type=my_view
+Sec-Fetch-Dest: empty
+Sec-Fetch-Mode: cors
+Sec-Fetch-Site: same-origin
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36
+X-Requested-With: XMLHttpRequest
+input_data: {"list_info":{"row_count":"25","start_index":"1","get_total_count":true,"sort_field":"scheduled_start_time","sort_order":"A","filter":"5"},"fields_required":["id","title","scheduled_start_time"]}
+SUBREQUEST: XMLHTTP
+_: 1602773274806
+
+
+
+
+Request URL: https://sd.korusconsulting.ru/api/v3/approvals?format=json&INPUT_DATA=%7B%22list_info%22%3A%7B%22filter%22%3A%7B%22name%22%3A%22MyPendingApprovals%22%7D%7D%7D&_=1602773274805
+Request Method: GET
+Status Code: 200 OK
+Remote Address: 91.231.213.53:443
+Referrer Policy: strict-origin-when-cross-origin
+Content-Length: 235
+Content-Type: application/json;charset=UTF-8
+Date: Thu, 15 Oct 2020 14:47:56 GMT
+Server: Apache-Coyote/1.1
+Accept: */*
+Accept-Encoding: gzip, deflate, br
+Accept-Language: en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7
+Connection: keep-alive
+Cookie: sdpcsrfcookie=41a67e47-e334-40ea-ac48-6b640030a3c5; SDPSESSIONID=A2FF8C6DFE774A5C764E100D04727532; JSESSIONIDSSO=FF8E356119E708D7625ECC6E90CB8806
+DNT: 1
+Host: sd.korusconsulting.ru
+Referer: https://sd.korusconsulting.ru/HomePage.do?view_type=my_view
+Sec-Fetch-Dest: empty
+Sec-Fetch-Mode: cors
+Sec-Fetch-Site: same-origin
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36
+X-Requested-With: XMLHttpRequest
+format: json
+INPUT_DATA: {"list_info":{"filter":{"name":"MyPendingApprovals"}}}
+_: 1602773274805
+
+
+
+Request URL: https://sd.korusconsulting.ru/api/v3/announcement?format=json&INPUT_DATA=%7B%22list_info%22%3A%7B%22row_count%22%3A25%2C%22search_fields%22%3A%7B%22filter%22%3A%22currently_showing%22%7D%2C%22sort_order%22%3A%22desc%22%2C%22sort_field%22%3A%22from_date%22%7D%7D&_=1602773274808
+Request Method: GET
+Status Code: 200 OK
+Remote Address: 91.231.213.53:443
+Referrer Policy: strict-origin-when-cross-origin
+Content-Length: 410
+Content-Type: application/json;charset=UTF-8
+Date: Thu, 15 Oct 2020 14:47:56 GMT
+Server: Apache-Coyote/1.1
+Accept: */*
+Accept-Encoding: gzip, deflate, br
+Accept-Language: en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7
+Connection: keep-alive
+Cookie: sdpcsrfcookie=41a67e47-e334-40ea-ac48-6b640030a3c5; SDPSESSIONID=A2FF8C6DFE774A5C764E100D04727532; JSESSIONIDSSO=FF8E356119E708D7625ECC6E90CB8806
+DNT: 1
+Host: sd.korusconsulting.ru
+Referer: https://sd.korusconsulting.ru/HomePage.do?view_type=my_view
+Sec-Fetch-Dest: empty
+Sec-Fetch-Mode: cors
+Sec-Fetch-Site: same-origin
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36
+X-Requested-With: XMLHttpRequest
+format: json
+INPUT_DATA: {"list_info":{"row_count":25,"search_fields":{"filter":"currently_showing"},"sort_order":"desc","sort_field":"from_date"}}
+_: 1602773274808
+
+
+Request URL: https://sd.korusconsulting.ru/api/v3/profile_pic/284701
+Request Method: GET
+Status Code: 200 OK
+Remote Address: 91.231.213.53:443
+Referrer Policy: strict-origin-when-cross-origin
+Cache-Control: max-age=0
+Content-disposition: attachment; filename="profilepic.jpg"
+Content-Encoding: gzip
+Content-Type: text/html
+Date: Thu, 15 Oct 2020 14:47:56 GMT
+Pragma: public
+Server: Apache-Coyote/1.1
+Transfer-Encoding: chunked
+Vary: Accept-Encoding
+Accept: image/avif,image/webp,image/apng,image/*,*/*;q=0.8
+Accept-Encoding: gzip, deflate, br
+Accept-Language: en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7
+Connection: keep-alive
+Cookie: sdpcsrfcookie=41a67e47-e334-40ea-ac48-6b640030a3c5; SDPSESSIONID=A2FF8C6DFE774A5C764E100D04727532; JSESSIONIDSSO=FF8E356119E708D7625ECC6E90CB8806
+DNT: 1
+Host: sd.korusconsulting.ru
+Referer: https://sd.korusconsulting.ru/HomePage.do?logout=true&logoutSkipNV2Filter=true
+Sec-Fetch-Dest: image
+Sec-Fetch-Mode: no-cors
+Sec-Fetch-Site: same-origin
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36
+
+
+
+
+Transfer-Encoding: chunked
+Vary: Accept-Encoding
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+Accept-Encoding: gzip, deflate, br
+Accept-Language: en-US,en;q=0.9,ru-RU;q=0.8,ru;q=0.7
+Cache-Control: max-age=0
+Connection: keep-alive
+Content-Length: 431
+Content-Type: application/x-www-form-urlencoded
+Cookie: sdpcsrfcookie=41a67e47-e334-40ea-ac48-6b640030a3c5; SDPSESSIONID=A2FF8C6DFE774A5C764E100D04727532; JSESSIONIDSSO=FF8E356119E708D7625ECC6E90CB8806
+DNT: 1
+Host: sd.korusconsulting.ru
+Origin: https://sd.korusconsulting.ru
+Referer: https://sd.korusconsulting.ru/WorkOrder.do?woMode=viewWO&woID=238179
+Sec-Fetch-Dest: iframe
+Sec-Fetch-Mode: navigate
+Sec-Fetch-Site: same-origin
+Sec-Fetch-User: ?1
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36
+taskid: null
+from: null
+chargeid:
+createdtime:
+createdby:
+module: request
+associatedEntity: request
+associatedEntityID: 238179
+scopeid: 309
+allowNullTimeEntry: true
+technicianid: 284701
+ts_starttime:
+total_timespent: 0
+timespenthrs:
+timespentmins: 0
+inc_nonOperationalHours: true
+ts_endtime: 1602774193307
+other_charge:
+worklogtypeid: null
+description: !тест!
+udfName3: -
+udfName1:
+udfName2:
+udfName11:
+submitact
