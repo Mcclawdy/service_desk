e4140fd814796af27d56fc35ac3a662145078588
McClawdy <mcclawdy@gmail.com>
2020-10-17 22:45:30 +0300

    new ui

---
+++ gui/MainWindow.py
@@ -0,0 +1,10 @@
+from PyQt5.QtCore import QDir, QFileInfo
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QlineEdit,
+    QFileDialog, QInputDialog, QMessageBox
+)
+from .ui.MainWindowUi import Ui_Form
+
+class MainWindow(QMainWindow):
+    pass
+
