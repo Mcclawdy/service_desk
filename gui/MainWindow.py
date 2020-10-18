45857bffcc333dca0b3045b53e26e9068396e61b
McClawdy <McClawdy@gmail.com>
2020-10-18 08:51:52 +0300

    new functions

--- gui/MainWindow.py
+++ gui/MainWindow.py
@@ -1,9 +1,9 @@
-from PyQt5.QtCore import QDir, QFileInfo
+from PyQt5.QtCore import QDir, QFileInfo, QTimer
 from PyQt5 import QtGui
 import requests
 from lxml import etree
 import xml.etree.ElementTree as ET
-import urllib
+
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QLineEdit,
     QFileDialog, QInputDialog, QMessageBox
@@ -12,22 +12,15 @@ from .ui.MainWindowUi import Ui_MainWindow
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
+        # self.requestAnsewer = None
         super().__init__(*args, **kwargs)
 
         self.setupUi(self)
         self._initSignals()
 
-
     def _initSignals(self):
         self.sendBtn.clicked.connect(self.sendWorklog)
-
-
-    def printError(self, data):
-        """Выдает сообщение об ошибке, если какие то данные не верны"""
-        ret = QMessageBox.warning(
-            self, f'Ошибка', f'Параметр {data} неверен'
-        )
-
+        self.logoutBtn.clicked.connect(self.close)
 
     def getEmployeeFullame(self):
         """Задает полное имя сотрудника"""
@@ -43,8 +36,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if self.requestDoubleSpinBox:
             request_number = int(self.requestDoubleSpinBox.value())
             return request_number
-        else:
-            print("request error")
 
 
     def getTimesheets(self):
@@ -52,16 +43,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if self.timesheetsDoubleSpinBox:
             timesheets = int(self.timesheetsDoubleSpinBox.value())
             return timesheets
-        else:
-            print("request error")
+
 
     def getComment(self):
         """Задает комментарий"""
         if self.commentEdit:
             comment = self.commentEdit.toPlainText()
             return comment
-        else:
-            print("request error")
+
 
     def parseXML(self):
         xml_str = f"""
@@ -97,28 +86,37 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         parser = etree.XMLParser(remove_blank_text=True)
         elem = etree.XML(xml_str, parser = parser)
-        print(type(etree.tostring(elem).decode("utf-8")))
         return etree.tostring(elem)
 
 
     def sendWorklog(self, data=None):
-        """Отправляет рабочий журнал, если все данные верны"""
-        print(self.parseXML())
-        headers = {'Content-Type': 'application/xml'}
+        """Отправляет рабочий журнал и возвращает ответ от сервера"""
         r = requests.post(f'https://sd.korusconsulting.ru/sdpapi/request/{self.getRequestNumber()}/worklogs',
-                          params={'OPERATION_NAME': 'ADD_WORKLOG', 'TECHNICIAN_KEY': '25F65F32-4E36-4F2A-B832-A08B8EBDAB6D', 'INPUT_DATA': self.parseXML()},
-)
-        print(r.url)
-        print(r.text)
+                          params={'OPERATION_NAME': 'ADD_WORKLOG',
+                                  'TECHNICIAN_KEY': '25F65F32-4E36-4F2A-B832-A08B8EBDAB6D',
+                                  'INPUT_DATA': self.parseXML()
+                                  },
+                        )
+        # self.requestAnsewer = r.text
 
 
     def checkData(self, request_number, employee_fullname):
         """Прооверяет статус заявки и наличие сотрудника"""
         pass
 
-    def closeApp(self):
-        """Выходит из приложения"""
+
+    # def parseRequestAnswer(self):
+    #     request = self.requestAnsewer
+    #     print(request)
+
+    def printError(self, data):
+        """Выдает сообщение об ошибке, если какие то данные не верны"""
+        ret = QMessageBox.warning(
+            self, f'Ошибка', f'Параметр {data} неверен'
+        )
+
+
+
+
+    def setStatusBar(self, statusbar: 'QStatusBar') -> None:
         pass
-
-
-
--- servicedesk.py
+++ servicedesk.py
@@ -2,9 +2,9 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from gui.MainWindow import MainWindow
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     mw = MainWindow()
     mw.show()
-
     sys.exit(app.exec_())
